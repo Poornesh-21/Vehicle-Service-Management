9e985b6fc42b97e542b4f3f24373f54ce9a15ec1
Poornesh-21 <poorneshsundravel@gmail.com>
2025-04-18 02:16:07 +0530

    dashbaord added

--- MVC/src/main/java/com/albany/mvc/dto/VehicleInServiceDTO.java
+++ MVC/src/main/java/com/albany/mvc/dto/VehicleInServiceDTO.java
@@ -4,19 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDate;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-class VehicleInServiceDTO {
+public class VehicleInServiceDTO {
     private Integer requestId;
     private String vehicleName;
     private String registrationNumber;
     private String serviceAdvisorName;
     private String serviceAdvisorId;
     private String status;
-    private String startDate;
-    private String estimatedCompletionDate;
+    private LocalDate startDate;        // Changed from String to LocalDate
+    private LocalDate estimatedCompletionDate;  // Changed from String to LocalDate
     private String category;
 }

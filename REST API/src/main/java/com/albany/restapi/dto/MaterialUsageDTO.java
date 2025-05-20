ce9b52c3ce2910d6e4e00cf24f4e7a9ecf618c14
Poornesh-21 <poorneshsundravel@gmail.com>
2025-05-21 01:28:51 +0530

    feat: inventory added

---
+++ REST API/src/main/java/com/albany/restapi/dto/MaterialUsageDTO.java
@@ -0,0 +1,21 @@
+package com.albany.restapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MaterialUsageDTO {
+    private Integer materialUsageId;
+    private String requestReference;
+    private LocalDateTime usedAt;
+    private BigDecimal quantity;
+    private String serviceAdvisorName;
+}
